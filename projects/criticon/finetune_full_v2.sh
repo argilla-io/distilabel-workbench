3135abc672f0de097c833a8392bbb32874604d48
plaguss <agustin@argilla.io>
2024-04-10 10:17:25 +0200

    Add script that created criticon

--- projects/criticon/finetune_full_v2.sh
+++ projects/criticon/finetune_full_v2.sh
@@ -1,7 +1,7 @@
 accelerate launch \
     --config_file configs/deepspeed-zero3.yaml \
     --num_processes 4 \
-    --no_python train sft --config-path config_full.yaml
+    --no_python train sft --config-path config_full_v2.yaml
 
 # Automatically remove the pod after finishing
 runpodctl remove pod $RUNPOD_POD_ID
